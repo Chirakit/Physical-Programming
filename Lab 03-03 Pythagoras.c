9942c25c6dbc379d7195ea2906ac2aa0759ae324
Chirakit Marachchalee <110179731+Chirakit@users.noreply.github.com>
2023-07-19 14:09:29 +0700

    Archive

---
+++ Lab 03-03 Pythagoras.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double num1, num2;
+    scanf("%lf %lf", &num1, &num2);
+    printf("%.2f", sqrt(pow(num1, 2) + pow(num2, 2)));
+    return 0;
+}
